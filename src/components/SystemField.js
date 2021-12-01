7e38dbe70b23118710bfa7264e95d755b549951e
Kyle <kylerobertduncan@gmail.com>
2021-12-01 16:41:48 -0500

    Engines system section renders and syncs

--- src/components/SystemField.js
+++ src/components/SystemField.js
@@ -1,7 +1,10 @@
+import database from '../firebase';
+import { update, ref } from 'firebase/database';
+
 const SystemField = (props) => {
 
   const { system, dataPath, headerText } = props;
-  
+
   /*
 
   render the fixed improvements w checkbox
@@ -13,6 +16,65 @@ const SystemField = (props) => {
   total the checked improvements for system count
 
   */
+
+  const decapitalize = ([first, ...rest]) => {
+    const newString = first.toLowerCase() + rest.join('');
+    return (newString.split(' ').join(''))
+  }
+  
+  const handleOptionChange = (e) => {
+    // get new values
+    const arrayIndex = +e.target.className;
+    const newValue = e.target.value
+    // build a new data array
+    const newDataArr = [ ...system.data ]
+    newDataArr[arrayIndex].labelText = newValue;
+    if (newValue !== "") {
+      newDataArr[arrayIndex].name = decapitalize(newValue);
+    } else {
+      newDataArr[arrayIndex].name = dataPath + arrayIndex;
+      newDataArr[arrayIndex].value = false;
+    }
+    // put the array inside a data object
+    const newDataObj = {
+      data: newDataArr
+    }
+    // update the information in firebase
+    const dataRef = ref(database, `main/${dataPath}`)
+    return(
+      update(dataRef, newDataObj)
+    )
+  }
+
+  const handleChange = (e) => {
+    // get relevant values
+    // console.log(e.target.name, e.target.value);
+    // build a new data array
+    const newDataArr = [...system.data]
+    // check data for appropriate match
+    newDataArr.forEach( (improvement, index) => {
+      // console.log(improvement);
+      if (improvement.name === e.target.name) {
+        if (improvement.labelText === "") {
+          alert("Select an improvement before equipping!")
+        } else {
+          improvement.value = e.target.checked
+          // console.log('changed at index', index);
+          // console.log(system.data[index]);
+          
+        }
+      }
+    })
+    const newDataObj = {
+      data: newDataArr
+    }
+
+    const dataRef = ref(database, `main/${dataPath}`);
+    return(
+      update(dataRef, newDataObj)
+    )
+    // console.log(newDataArr);
+  }
 
   return(
     <section className={dataPath}>
@@ -27,11 +89,11 @@ const SystemField = (props) => {
           const { fixed, labelText, name, value } = improvement;
           if (fixed) {
             return(
-              <li>
+              <li key={name}>
                 <input
                   checked={value}
                   name={name}
-                  // onChange={handleChange}
+                  onChange={handleChange}
                   type="checkbox"
                 />
                 <label htmlFor={name}>{labelText}</label>
@@ -39,21 +101,25 @@ const SystemField = (props) => {
             )
           } else {
             return(
-              <li>
+              <li key={name}>
                 <input
                   checked={value}
                   name={name}
-                  // onChange={handleChange}
+                  onChange={handleChange}
                   type="checkbox"
                 />
-                <select name={name}>
-                  <option value="">Empty slot</option>
+                <select
+                  className={index}
+                  name={name}
+                  onChange={handleOptionChange}
+                  value={labelText}
+                >
+                  <option value="" >Empty slot</option>
                   {
                     system?.options.map( (option) => {
-                      // console.log(option);
-                      const optionName = option.split(" ").join("");
+                      const optionName = decapitalize(option)
                       return(
-                        <option value={optionName}>{option}</option>
+                        <option key={optionName} value={option}>{option}</option>
                       )
                     })
                   }
